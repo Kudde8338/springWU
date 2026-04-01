0803630cfc12b9abb2b6fd87ad3a99e809637b76
Sydney Myrén <sydney.myren@gmail.com>
2026-04-01 13:15:15 +0200

    fixed hamburger menu for mobile version of header

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function hamburger() {
+  var x = document.getElementById("pages-phone");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
